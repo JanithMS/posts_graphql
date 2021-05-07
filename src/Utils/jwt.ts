ccb702924ea5213ba24dd53e3ab1c96049965e44
JanithMS <60615623+JanithMS@users.noreply.github.com>
2021-05-07 19:28:42 +0530

    Update jwt.ts

--- src/Utils/jwt.ts
+++ src/Utils/jwt.ts
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
 
 export default function jwtToken(user_id: number, user_password: String) {
-    return jwt.sign({_id: user_id, _password: user_password}, ""+process.env.jwtSecret, { expiresIn: '30days'})
+    return jwt.sign({_id: user_id, _password: user_password}, ""+process.env.JWT_SECRET, { expiresIn: '30days'})
 }
